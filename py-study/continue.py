aa1aeb596c111a808a5a97d9bd2d3a029e5eb13a
Biglazy <qihongchuan@gmail.com>
2017-10-11 00:07:00 +0800

    简明python教程：break语句和continue语句

---
+++ py-study/continue.py
@@ -0,0 +1,9 @@
+while True:
+    s = input('Enter something: ')
+    if s == 'quit':
+        break
+    if len(s) < 3:
+        print('Too small')
+        continue
+    print('Input is of sufficient length')
+print('Done')
